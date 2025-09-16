0e542a75536b333b0479780125f650a31acaf42d
Shreya <shreyashinde883@gmail.com>
2025-09-16 22:03:55 +0530

    navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,17 @@
-import React from 'react'
-import { Route, Routes } from 'react-router';
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import CreatePage from './pages/CreatePage';
 import NoteDetailPage from './pages/NoteDetailPage';
-import { toast } from 'react-hot-toast';
 
 const App = () => {
-  return (
-
-   <div>
-     
-
-     <Routes> 
-        <Route path='/' element={<div>HomePage</div>} />
-        <Route path='/create' element={<div>CreatePage</div>} />
-        <Route path='/note/:id' element={<div>NoteDetailPage</div>} />  
-
-     </Routes>
-
+  return ( 
+    <div data-theme="coffee">
+      <Routes> 
+        <Route path='/' element={<HomePage />} />
+        <Route path='/create' element={<CreatePage />} />
+        <Route path='/note/:id' element={<NoteDetailPage />} />  
+      </Routes>
     </div>
   );
 };
