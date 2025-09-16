52ba1f1e461462f364fc09698500936ff72cc5fe
Shreya <shreyashinde883@gmail.com>
2025-09-16 16:07:04 +0530

    daisyui import

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import { Route, Routes } from 'react-router';
+import HomePage from './pages/HomePage';
+import CreatePage from './pages/CreatePage';
+import NoteDetailPage from './pages/NoteDetailPage';
+import { toast } from 'react-hot-toast';
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+
+   <div>
+     
+
+     <Routes> 
+        <Route path='/' element={<div>HomePage</div>} />
+        <Route path='/create' element={<div>CreatePage</div>} />
+        <Route path='/note/:id' element={<div>NoteDetailPage</div>} />  
+
+     </Routes>
+
+    </div>
+  );
+};
 
 export default App;
